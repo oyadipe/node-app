d3754f2ca6876383bb1498905f16668576958849
Oyadipe Yinka Oluwagbenga <32593222+oyadipe@users.noreply.github.com>
2026-05-18 20:55:28 -0400

    more commit
    to test the ci/cd again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 DevOps Capstone App is Running test is live and direct!");
+  res.send("🚀 DevOps Capstone App is Running test is live and direct ilove what am seeing!");
 });
 
 app.get("/health", (req, res) => {
